bf264f11fafb552a93eeec846485256d11050604
JakeRoughton <112968037+JakeRoughton@users.noreply.github.com>
2022-10-24 15:34:46 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+let para = document.querySelector('p');
+para.style.color = "lightblue"
+para.innerText = "Lorem ipsum dolor amet viral meh selfies drinking vinegar, intelligentsia poke flannel twee paleo enamel pin cray. Banjo celiac crucifix, kickstarter la croix air plant jianbing hashtag vinyl hell of man bun selvage schlitz banh mi. Tacos hella raclette quinoa blog, williamsburg adaptogen tbh. Hexagon af stumptown lumbersexual synth gentrify quinoa enamel pin celiac master cleanse. Truffaut typewriter shoreditch, semiotics iceland mixtape taxidermy umami distillery austin hashtag. Food truck synth wayfarers, street art banh mi actually authentic. Bitters tousled tattooed vegan neutra pug hell of fixie chia unicorn letterpress."
+
+let head = document.querySelector('h1');
+head.style.fontSize = "48px"
+
+let item13 = document.querySelector("#item-13")
+item13.style.opacity = ".3"
+
+let item3 = document.querySelector("#item-3")
+item3.innerText = "Hi"
+
+let image = document.querySelector("img")
+image.src = "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/a3612a13-3a4b-4c9e-adb1-5a0f60091611/dbcjt0k-540930dc-f62f-49b3-a495-344d29b616fb.png/v1/fill/w_1268,h_630,q_70,strp/space_engineers__journey_2_by_ivanlaliashvili_dbcjt0k-pre.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9Nzk1IiwicGF0aCI6IlwvZlwvYTM2MTJhMTMtM2E0Yi00YzllLWFkYjEtNWEwZjYwMDkxNjExXC9kYmNqdDBrLTU0MDkzMGRjLWY2MmYtNDliMy1hNDk1LTM0NGQyOWI2MTZmYi5wbmciLCJ3aWR0aCI6Ijw9MTYwMCJ9XV0sImF1ZCI6WyJ1cm46c2VydmljZTppbWFnZS5vcGVyYXRpb25zIl19.j5lqWctYQpPKNWPHCvPBPDNcRsAxmsUcpl2Jp-SiGB8"
+image.style.height = "300px"
+
+let item16 = document.querySelector("#item-16");
+item16.className = "item"
+item16.innerText = "Won't get fooled again." 
+
+// let p.innerText = "Lorem ipsum dolor amet viral meh selfies drinking vinegar, intelligentsia poke flannel twee paleo enamel pin cray. Banjo celiac crucifix, kickstarter la croix air plant jianbing hashtag vinyl hell of man bun selvage schlitz banh mi. Tacos hella raclette quinoa blog, williamsburg adaptogen tbh. Hexagon af stumptown lumbersexual synth gentrify quinoa enamel pin celiac master cleanse. Truffaut typewriter shoreditch, semiotics iceland mixtape taxidermy umami distillery austin hashtag. Food truck synth wayfarers, street art banh mi actually authentic. Bitters tousled tattooed vegan neutra pug hell of fixie chia unicorn letterpress."
