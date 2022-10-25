2a346b10d37ca7caf677707375aa2ba032c16050
JakeRoughton <112968037+JakeRoughton@users.noreply.github.com>
2022-10-25 10:55:01 -0400

    Update main.js

--- main.js
+++ main.js
@@ -22,5 +22,3 @@ image.style.height = "300px"
 let item16 = document.querySelector("#item-16");
 item16.className = "item"
 item16.innerText = "Won't get fooled again." 
-
-// let p.innerText = "Lorem ipsum dolor amet viral meh selfies drinking vinegar, intelligentsia poke flannel twee paleo enamel pin cray. Banjo celiac crucifix, kickstarter la croix air plant jianbing hashtag vinyl hell of man bun selvage schlitz banh mi. Tacos hella raclette quinoa blog, williamsburg adaptogen tbh. Hexagon af stumptown lumbersexual synth gentrify quinoa enamel pin celiac master cleanse. Truffaut typewriter shoreditch, semiotics iceland mixtape taxidermy umami distillery austin hashtag. Food truck synth wayfarers, street art banh mi actually authentic. Bitters tousled tattooed vegan neutra pug hell of fixie chia unicorn letterpress."
